14b019934f8fa1a0a459f5da94f973caa42f4f09
Brad Davies <me@varbrad.dev>
2022-12-04 16:57:22 +0000

    Refactor

--- days/day4.ts
+++ days/day4.ts
@@ -6,16 +6,11 @@ const regex = /^(\d+)-(\d+),(\d+)-(\d+)$/
 const format = (input: string) =>
   split(input).map((l) => l.match(regex)!.slice(1, 5).map(Number))
 
-export const part1 = (input: string) =>
-  sum(
-    format(input).map(([a0, a1, b0, b1]) =>
-      (a0 >= b0 && a1 <= b1) || (b0 >= a0 && b1 <= a1) ? 1 : 0
-    )
-  )
+const contains = ([a0, a1, b0, b1]: number[]) =>
+  (a0 >= b0 && a1 <= b1) || (b0 >= a0 && b1 <= a1) ? 1 : 0
 
-export const part2 = (input: string) =>
-  sum(
-    format(input).map(([a0, a1, b0, b1]) =>
-      (a0 >= b0 && a0 <= b1) || (b0 >= a0 && b0 <= a1) ? 1 : 0
-    )
-  )
+const overlaps = ([a0, a1, b0, b1]: number[]) =>
+  (a0 >= b0 && a0 <= b1) || (b0 >= a0 && b0 <= a1) ? 1 : 0
+
+export const part1 = (input: string) => sum(format(input).map(contains))
+export const part2 = (input: string) => sum(format(input).map(overlaps))
