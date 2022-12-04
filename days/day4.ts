c71576ec9a0abc11ab8412c44721cd9642c06c79
Brad Davies <me@varbrad.dev>
2022-12-04 17:05:08 +0000

    Day 4 - refactor

--- days/day4.ts
+++ days/day4.ts
@@ -1,4 +1,3 @@
-import { sum } from '../utils/array'
 import { split } from '../utils/string'
 
 const regex = /^(\d+)-(\d+),(\d+)-(\d+)$/
@@ -7,10 +6,10 @@ const format = (input: string) =>
   split(input).map((l) => l.match(regex)!.slice(1, 5).map(Number))
 
 const contains = ([a0, a1, b0, b1]: number[]) =>
-  (a0 >= b0 && a1 <= b1) || (b0 >= a0 && b1 <= a1) ? 1 : 0
+  (a0 >= b0 && a1 <= b1) || (b0 >= a0 && b1 <= a1)
 
 const overlaps = ([a0, a1, b0, b1]: number[]) =>
-  (a0 >= b0 && a0 <= b1) || (b0 >= a0 && b0 <= a1) ? 1 : 0
+  (a0 >= b0 && a0 <= b1) || (b0 >= a0 && b0 <= a1)
 
-export const part1 = (input: string) => sum(format(input).map(contains))
-export const part2 = (input: string) => sum(format(input).map(overlaps))
+export const part1 = (input: string) => format(input).filter(contains).length
+export const part2 = (input: string) => format(input).filter(overlaps).length
