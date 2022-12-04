eb9de6c586db064d72bad200efe939b09b64e5e8
Brad Davies <me@varbrad.dev>
2022-12-04 17:08:57 +0000

    Day 4 - More code golfing

--- days/day4.ts
+++ days/day4.ts
@@ -1,14 +1,12 @@
 import { split } from '../utils/string'
 
-const regex = /^(\d+)-(\d+),(\d+)-(\d+)$/
-
 const format = (input: string) =>
-  split(input).map((l) => l.match(regex)!.slice(1, 5).map(Number))
+  split(input).map((l) => l.split(',').map((i) => i.split('-').map(Number)))
 
-const contains = ([a0, a1, b0, b1]: number[]) =>
+const contains = ([[a0, a1], [b0, b1]]: number[][]) =>
   (a0 >= b0 && a1 <= b1) || (b0 >= a0 && b1 <= a1)
 
-const overlaps = ([a0, a1, b0, b1]: number[]) =>
+const overlaps = ([[a0, a1], [b0, b1]]: number[][]) =>
   (a0 >= b0 && a0 <= b1) || (b0 >= a0 && b0 <= a1)
 
 export const part1 = (input: string) => format(input).filter(contains).length
