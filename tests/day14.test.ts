c9915e8c9bbd5fa90eb2ea99c65740a76c4bd789
Brad Davies <brad.davies@withwise.com>
2022-12-14 09:00:13 +0000

    Day 14 - Input cases

--- tests/day14.test.ts
+++ tests/day14.test.ts
@@ -1,12 +1,18 @@
 import { describe, expect, it } from 'vitest'
 
 import * as day14 from '../days/day14'
+import { readDayInput } from '../utils/io'
 
 describe('D14P1', () => {
   it('should solve example case', () => {
     const input = `498,4 -> 498,6 -> 496,6
 503,4 -> 502,4 -> 502,9 -> 494,9`
     expect(day14.part1(input)).toEqual(24)
+  })
+
+  it('should solve input case', () => {
+    const input = readDayInput(14)
+    expect(day14.part1(input)).toEqual(672)
   })
 })
 
@@ -15,5 +21,10 @@ describe('D14P2', () => {
     const input = `498,4 -> 498,6 -> 496,6
   503,4 -> 502,4 -> 502,9 -> 494,9`
     expect(day14.part2(input)).toEqual(93)
+  })
+
+  it('should solve input case', () => {
+    const input = readDayInput(14)
+    expect(day14.part2(input)).toEqual(26831)
   })
 })
