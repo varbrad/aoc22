fa6a842033c50d3b7fed4ab88acf277a3e35d22c
Brad Davies <me@varbrad.dev>
2022-12-18 14:05:37 +0000

    Cache for checking cells already searched

--- days/day18.ts
+++ days/day18.ts
@@ -1,30 +1,28 @@
+import { first } from 'lodash'
+import { array } from '../utils/array'
 import { minmax } from '../utils/math'
 import { split } from '../utils/string'
 
 const solve = (input: string) => {
   const map = new Set<string>()
-  let count = 0
-  split(input).forEach((row) => {
+  const count = split(input).reduce((prev, row) => {
     const [x, y, z] = row.split(',').map(Number)
-    let faces = 6
-    const left = map.has(`${x - 1},${y},${z}`)
-    const right = map.has(`${x + 1},${y},${z}`)
-    const top = map.has(`${x},${y - 1},${z}`)
-    const bottom = map.has(`${x},${y + 1},${z}`)
-    const front = map.has(`${x},${y},${z - 1}`)
-    const back = map.has(`${x},${y},${z + 1}`)
-
-    if (left) faces -= 2
-    if (right) faces -= 2
-    if (top) faces -= 2
-    if (bottom) faces -= 2
-    if (front) faces -= 2
-    if (back) faces -= 2
-
-    count += faces
-
-    map.add(`${x},${y},${z}`)
-  })
+    map.add(row)
+    return (
+      prev +
+      array(6).reduce<number>(
+        (prev, i) =>
+          map.has(
+            `${x + (i === 0 ? -1 : i === 1 ? 1 : 0)},${
+              y + (i === 2 ? -1 : i === 3 ? 1 : 0)
+            },${z + (i === 4 ? -1 : i === 5 ? 1 : 0)}`
+          )
+            ? prev - 2
+            : prev,
+        6
+      )
+    )
+  }, 0)
 
   return [map, count] as const
 }
@@ -51,26 +49,42 @@ const pruneToInterior = (
   )
     return 'outside'
   startSet.add(`${x},${y},${z}`)
-  const a = pruneToInterior(map, xs, ys, zs, x - 1, y, z, startSet)
-  if (a === 'outside') return 'outside'
-  const b = pruneToInterior(map, xs, ys, zs, x + 1, y, z, startSet)
-  if (b === 'outside') return 'outside'
-  const c = pruneToInterior(map, xs, ys, zs, x, y - 1, z, startSet)
-  if (c === 'outside') return 'outside'
-  const d = pruneToInterior(map, xs, ys, zs, x, y + 1, z, startSet)
-  if (d === 'outside') return 'outside'
-  const e = pruneToInterior(map, xs, ys, zs, x, y, z - 1, startSet)
-  if (e === 'outside') return 'outside'
-  const f = pruneToInterior(map, xs, ys, zs, x, y, z + 1, startSet)
-  if (f === 'outside') return 'outside'
-
+  for (let i = 0; i < 6; i++) {
+    const [ax, ay, az] = [
+      x + (i === 0 ? -1 : i === 1 ? 1 : 0),
+      y + (i === 2 ? -1 : i === 3 ? 1 : 0),
+      z + (i === 4 ? -1 : i === 5 ? 1 : 0),
+    ]
+    const res = pruneToInterior(map, xs, ys, zs, ax, ay, az, startSet)
+    if (res === 'outside') return 'outside'
+  }
   return startSet
+}
+
+const floodFill = (
+  map: Set<string>,
+  x: number,
+  y: number,
+  z: number,
+  set: Set<string>
+) => {
+  if (map.has(`${x},${y},${z}`) || set.has(`${x},${y},${z}`)) return set
+  set.add(`${x},${y},${z}`)
+  for (let i = 0; i < 6; i++) {
+    const [ax, ay, az] = [
+      x + (i === 0 ? -1 : i === 1 ? 1 : 0),
+      y + (i === 2 ? -1 : i === 3 ? 1 : 0),
+      z + (i === 4 ? -1 : i === 5 ? 1 : 0),
+    ]
+    floodFill(map, ax, ay, az, set)
+  }
+  return set
 }
 
 export const part1 = (input: string) => solve(input)[1]
 
 export const part2 = (input: string) => {
-  const [map, totalSurface] = solve(input)
+  const [map] = solve(input)
   const mapValues = Array.from(map.values())
   const xs = minmax(...mapValues.map((v) => Number(v.split(',')[0])))
   const ys = minmax(...mapValues.map((v) => Number(v.split(',')[1])))
@@ -86,13 +100,16 @@ export const part2 = (input: string) => {
     }
   }
 
-  const firstVoid = Array.from(voidMap.values()).filter((v) => {
+  const knownInsidePoints = new Set<string>()
+  const interiorPoints = Array.from(voidMap.values()).filter((v) => {
+    if (knownInsidePoints.has(v)) return true
     const [x, y, z] = v.split(',').map(Number)
     const result = pruneToInterior(map, xs, ys, zs, x, y, z, new Set<string>())
-    if (result !== 'outside') return true
-    return false
+    if (result === 'outside') return false
+    result.forEach((v) => knownInsidePoints.add(v))
+    return true
   })
 
-  firstVoid.forEach((v) => map.add(v))
+  interiorPoints.forEach((v) => map.add(v))
   return solve(Array.from(map.values()).join('\n'))[1]
 }
