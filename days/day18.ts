d79bf4c690cde6ace5f53a292004e411ea3fd2f8
Brad Davies <me@varbrad.dev>
2022-12-18 14:09:44 +0000

    Day 18 - refactor

--- days/day18.ts
+++ days/day18.ts
@@ -3,7 +3,7 @@ import { array } from '../utils/array'
 import { minmax } from '../utils/math'
 import { split } from '../utils/string'
 
-const solve = (input: string) => {
+const getSurfaceArea = (input: string) => {
   const map = new Set<string>()
   const count = split(input).reduce((prev, row) => {
     const [x, y, z] = row.split(',').map(Number)
@@ -24,10 +24,10 @@ const solve = (input: string) => {
     )
   }, 0)
 
-  return [map, count] as const
+  return [count, map] as const
 }
 
-const pruneToInterior = (
+const isInterior = (
   map: Set<string>,
   xs: [number, number],
   ys: [number, number],
@@ -35,7 +35,7 @@ const pruneToInterior = (
   x: number,
   y: number,
   z: number,
-  startSet: Set<string>
+  startSet: Set<string> = new Set<string>()
 ): Set<string> | 'outside' => {
   if (map.has(`${x},${y},${z}`) || startSet.has(`${x},${y},${z}`))
     return startSet
@@ -55,36 +55,16 @@ const pruneToInterior = (
       y + (i === 2 ? -1 : i === 3 ? 1 : 0),
       z + (i === 4 ? -1 : i === 5 ? 1 : 0),
     ]
-    const res = pruneToInterior(map, xs, ys, zs, ax, ay, az, startSet)
+    const res = isInterior(map, xs, ys, zs, ax, ay, az, startSet)
     if (res === 'outside') return 'outside'
   }
   return startSet
 }
 
-const floodFill = (
-  map: Set<string>,
-  x: number,
-  y: number,
-  z: number,
-  set: Set<string>
-) => {
-  if (map.has(`${x},${y},${z}`) || set.has(`${x},${y},${z}`)) return set
-  set.add(`${x},${y},${z}`)
-  for (let i = 0; i < 6; i++) {
-    const [ax, ay, az] = [
-      x + (i === 0 ? -1 : i === 1 ? 1 : 0),
-      y + (i === 2 ? -1 : i === 3 ? 1 : 0),
-      z + (i === 4 ? -1 : i === 5 ? 1 : 0),
-    ]
-    floodFill(map, ax, ay, az, set)
-  }
-  return set
-}
-
-export const part1 = (input: string) => solve(input)[1]
+export const part1 = (input: string) => getSurfaceArea(input)[0]
 
 export const part2 = (input: string) => {
-  const [map] = solve(input)
+  const [, map] = getSurfaceArea(input)
   const mapValues = Array.from(map.values())
   const xs = minmax(...mapValues.map((v) => Number(v.split(',')[0])))
   const ys = minmax(...mapValues.map((v) => Number(v.split(',')[1])))
@@ -94,8 +74,7 @@ export const part2 = (input: string) => {
   for (let z = zs[0]; z < zs[1]; z++) {
     for (let y = ys[0]; y < ys[1]; y++) {
       for (let x = xs[0]; x < xs[1]; x++) {
-        const isVoid = map.has(`${x},${y},${z}`) === false
-        if (isVoid) voidMap.add(`${x},${y},${z}`)
+        if (!map.has(`${x},${y},${z}`)) voidMap.add(`${x},${y},${z}`)
       }
     }
   }
@@ -104,12 +83,12 @@ export const part2 = (input: string) => {
   const interiorPoints = Array.from(voidMap.values()).filter((v) => {
     if (knownInsidePoints.has(v)) return true
     const [x, y, z] = v.split(',').map(Number)
-    const result = pruneToInterior(map, xs, ys, zs, x, y, z, new Set<string>())
+    const result = isInterior(map, xs, ys, zs, x, y, z)
     if (result === 'outside') return false
     result.forEach((v) => knownInsidePoints.add(v))
     return true
   })
 
   interiorPoints.forEach((v) => map.add(v))
-  return solve(Array.from(map.values()).join('\n'))[1]
+  return getSurfaceArea(Array.from(map.values()).join('\n'))[0]
 }
